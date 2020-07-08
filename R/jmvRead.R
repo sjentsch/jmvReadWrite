d1ee69a423a36a0d7afe997a06750c9c534f2315
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2020-07-08 13:35:47 +0200

    2020-07-07_1

--- R/jmvRead.R
+++ R/jmvRead.R
@@ -1,19 +1,19 @@
-jmvRead <- function(fleNme = "", useFlt = FALSE) {
+jmvRead <- function(fleNme = "", useFlt = FALSE, sveAtt = FALSE) {
 # read files created of jamovi (statistical spreadsheet, www.jamovi.org)
 # fleNme - name of the jamovi file (.omv; default = "")
 # useFlt - apply filters (default: FALSE; i.e., remove the lines where the filter is set to 0)
+# sveAtt - store attributes that are not required in the data set
 # 
 # (C) Sebastian Jentschke, 2020-
 # License: GPL-3
   
     # check whether the file exists and whether it has the correct format
-    if (! file.exists(fleNme))                                                    { stop(paste('File', fleNme, 'not found.')) }
+    if (! file.exists(fleNme))                                                    { stop(paste0('File "', fleNme, '" not found.')) }
     hdrStr = readBin(tmpHdl <- file(fleNme, 'rb'), 'character'); close(tmpHdl); rm('tmpHdl');
-    if (! hdrStr == "PK\003\004\024")                                             { stop(paste('File', fleNme, 'has not the correct file format (is not a ZIP archive).')) }
-    if (! grepl('META-INF/MANIFEST.MF', toString(unzip(fleNme, list=TRUE)$Name))) { stop(paste('File', fleNme, 'has not the correct file format (is not a jamovi-file).')) }
+    if (! hdrStr == "PK\003\004\024")                                             { stop(paste0('File "', fleNme, '" has not the correct file format (is not a ZIP archive).')) }
+    if (! grepl('META-INF/MANIFEST.MF', toString(unzip(fleNme, list=TRUE)$Name))) { stop(paste0('File "', fleNme, '" has not the correct file format (is not a jamovi-file).')) }
     
     library(rjson)
-    
 
     mnfTxt <-          readLines(mnfHdl <- file(mnfFle <- unzip(fleNme, 'META-INF/MANIFEST.MF', junkpaths = T), 'r'), warn=F);  close(mnfHdl); unlink(mnfFle); rm('mnfHdl', 'mnfFle');
     mtaDta <- fromJSON(readLines(mtaHdl <- file(mtaFle <- unzip(fleNme, 'metadata.json',        junkpaths = T), 'r'), warn=F)); close(mtaHdl); unlink(mtaFle); rm('mtaHdl', 'mtaFle');
@@ -22,6 +22,7 @@ jmvRead <- function(fleNme = "", useFlt = FALSE) {
     # TO-DO: import HTML output - index.html
    
     # process meta-data
+    if (names(mtaDta) != "dataSet") { stop('Unimplemeted field in the meta data') }
     # rowCount, columnCount
     rowNum = mtaDta$dataSet$rowCount
     colNum = mtaDta$dataSet$columnCount
@@ -29,8 +30,8 @@ jmvRead <- function(fleNme = "", useFlt = FALSE) {
     if (length(mtaDta$dataSet$removedRows) > 0 || length(mtaDta$dataSet$addedRows) > 0) { stop('addedRow or removedRows not empty: Needs to be implemented') }
 
     # fields
-    nmeVec = c()
-    lblVec = c()
+    nmeLst = c()
+    lblLst = c()
     fltLst = c()
     for (i in 1:length(mtaDta$dataSet$fields)) {
         # type: determines the format in the binary file
@@ -42,38 +43,54 @@ jmvRead <- function(fleNme = "", useFlt = FALSE) {
             stop(paste('Variable type', mtaDta$dataSet$fields[[i]]$type, 'not implemented.'))
         }
         
-        # name, description: same as foreign
-        nmeVec = c(nmeVec, mtaDta$dataSet$fields[[i]]$name)
-        lblVec = c(lblVec, mtaDta$dataSet$fields[[i]]$description)
-        if (lblVec[[i]] != "")
-            attr(colRaw[[1]], 'jmv-desc') = mtaDta$dataSet$fields[[i]]$description
+        # name, description
+        nmeCrr = mtaDta$dataSet$fields[[i]]$name
+        lblCrr = mtaDta$dataSet$fields[[i]]$description        
+        nmeLst = c(nmeLst, nmeCrr)
+        lblLst = c(lblLst, lblCrr)
+        if (lblCrr != "")
+            attr(colRaw[[1]], 'jmv-desc') = lblCrr
         # dataType - value labels
-        if (any(grepl(nmeVec[[i]], names(xtdDta)))) {
+        if (any(grepl(nmeCrr, names(xtdDta)))) {
             if (mtaDta$dataSet$fields[[i]]$dataType == 'Text') {
-                colRaw[[1]] = factor(colRaw[[1]], levels = unlist(sapply(xtdDta[[nmeVec[[i]]]]$labels, function(m) m[1])), labels = unlist(sapply(xtdDta[[nmeVec[[i]]]]$labels, function(m) m[2])))
+                colRaw[[1]] = factor(colRaw[[1]], levels = unlist(sapply(xtdDta[[nmeCrr]]$labels, function(m) m[1])), labels = unlist(sapply(xtdDta[[nmeCrr]]$labels, function(m) m[2])))
             } else if (mtaDta$dataSet$fields[[i]]$columnType == 'Recoded') {
-                colRaw[[1]] = factor(colRaw[[1]], levels = unlist(sapply(xtdDta[[nmeVec[[i]]]]$labels, function(m) m[1])), labels = unlist(sapply(xtdDta[[nmeVec[[i]]]]$labels, function(m) m[2])))
-                attr(colRaw[[1]], 'values') = unlist(sapply(xtdDta[[nmeVec[[i]]]]$labels, function(m) as.integer(m[1])))
+                colRaw[[1]] = factor(colRaw[[1]], levels = unlist(sapply(xtdDta[[nmeCrr]]$labels, function(m) m[1])), labels = unlist(sapply(xtdDta[[nmeCrr]]$labels, function(m) m[2])))
+                attr(colRaw[[1]], 'values') = unlist(sapply(xtdDta[[nmeCrr]]$labels, function(m) as.integer(m[1])))
             } else if (mtaDta$dataSet$fields[[i]]$columnType == 'Filter') {
-                colRaw[[1]] = factor(colRaw[[1]], levels = unlist(sapply(xtdDta[[nmeVec[[i]]]]$labels, function(m) m[1])), labels = unlist(sapply(xtdDta[[nmeVec[[i]]]]$labels, function(m) m[2])))
-                attr(colRaw[[1]], 'values') = unlist(sapply(xtdDta[[nmeVec[[i]]]]$labels, function(m) as.integer(m[1])))
+                colRaw[[1]] = as.logical(colRaw[[1]])
+                fltLst = c(fltLst, i)
             } else {
-# Use the factor() function for nominal data and the ordered() function for ordinal data. R      
-#mydata$y <- ordered(mydata$y,
-#levels = c(1,3, 5),
-#labels = c("Low", "Medium", "High"))
-                print(paste(nmeVec[[i]], ' - ', mtaDta$dataSet$fields[[i]]$dataType, ' - ', mtaDta$dataSet$fields[[i]]$columnType))
+                print('HERE')
+                print(paste(nmeCrr, ' - ', mtaDta$dataSet$fields[[i]]$dataType, ' - ', mtaDta$dataSet$fields[[i]]$columnType))
             }
         }
-
         
         # TO-DO: id, columnType, measureType, formula, formulaMessage, parentId, width, importName, transform, edits, missingValues, trimLevels
-        if (i == 1) { names(colRaw) = nmeVec[[i]]; dtaFrm = colRaw } else { dtaFrm[nmeVec[[i]]] = colRaw }       
+        str(colRaw)
+        if (i == 1) { names(colRaw) = nmeCrr; dtaFrm = colRaw } else { dtaFrm[nmeCrr] = colRaw }
+        print(i)
+        str(dtaFrm)
         rm('colRaw')
     }
-    # handle variable labels
-    names(lblVec) = nmeVec; attr(dtaFrm, 'variable.labels') = lblVec; rm('nmeVec', 'lblVec');
     
+    # handle filters
+    if (useFlt) {
+        fltInc = rep(TRUE, dim(dtaFrm)[1])
+        for (i in fltLst) fltInc = fltInc & dtaFrm[[i]]
+        print(str(dtaFrm))
+        dtaFrm = dtaFrm[fltInc, ]
+        print(str(dtaFrm))
+#       dtaFrm[fltLst] = NULL
+        print(str(dtaFrm))
+    } else {
+        attr(dtaFrm, 'fltLst') = nmeLst[fltLst]
+    }   
+    
+    print(lblLst);
+    # handle variable labels
+    names(lblLst) = nmeLst; attr(dtaFrm, 'variable.labels') = lblLst; rm('nmeLst', 'lblLst');   
+            
     # TO-DO: convert transforms into attributes
     # 'name', 'id', 'suffix', 'formula', 'formulaMessage', 'measureType', 'description'
     

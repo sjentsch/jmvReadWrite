86ba044da6ece56c054b44cb7e6f2d16a8404764
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2020-08-12 22:42:10 +0200

    Prepare for CRAN

--- R/jmvRead.R
+++ R/jmvRead.R
@@ -1,11 +1,14 @@
-jmvRead <- function(fleNme = "", useFlt = FALSE, sveAtt = FALSE) {
-# read files created of jamovi (statistical spreadsheet, www.jamovi.org)
-# fleNme - name of the jamovi file (.omv; default = "")
-# useFlt - apply filters (default: FALSE; i.e., remove the lines where the filter is set to 0)
-# sveAtt - store attributes that are not required in the data set
-# 
-# (C) Sebastian Jentschke, 2020-
-# License: GPL-3
+#' Read files created of jamovi (statistical spreadsheet, www.jamovi.org)
+#'
+#' @param fleNme name of the jamovi file to be read (.omv; default = "")
+#' @param useFlt apply filters (remove the lines where the filter is set to 0; default: FALSE)
+#' @param rmMsVl remove values defined as missing values (replace them with NA; default - FALSE)
+#' @param sveAtt store attributes that are not required in the data set (if you want to write the same data set using jmvWrite; default – FALSE)
+#' @return data frame (can be directly used with functions included in the R-package "jmv" and syntax from jamovi; also compatible with the format of the  R-package "foreign")
+#'
+#' @export jmvRead
+
+jmvRead <- function(fleNme = "", useFlt = FALSE, rmMsVl = FALSE, sveAtt = FALSE) {
   
     # check whether the file exists and whether it has the correct format
     if (! file.exists(fleNme))                                                    { stop(paste0('File "', fleNme, '" not found.')) }
@@ -13,93 +16,144 @@ jmvRead <- function(fleNme = "", useFlt = FALSE, sveAtt = FALSE) {
     if (! hdrStr == "PK\003\004\024")                                             { stop(paste0('File "', fleNme, '" has not the correct file format (is not a ZIP archive).')) }
     if (! grepl('META-INF/MANIFEST.MF', toString(unzip(fleNme, list=TRUE)$Name))) { stop(paste0('File "', fleNme, '" has not the correct file format (is not a jamovi-file).')) }
     
-    library(rjson)
+    # check whether the archive contains a string.bin-file (it only exists if there are columns that contain text variables)
+    strBin = grepl('strings.bin', toString(unzip(fleNme, list=TRUE)$Name))
+    
+    # read and decode
+    mnfTxt <-                 readLines(mnfHdl <- file(mnfFle <- unzip(fleNme, 'META-INF/MANIFEST.MF', junkpaths = T), 'r'), warn=F);  close(mnfHdl); unlink(mnfFle); rm('mnfHdl', 'mnfFle');
+    mtaDta <- rjson::fromJSON(readLines(mtaHdl <- file(mtaFle <- unzip(fleNme, 'metadata.json',        junkpaths = T), 'r'), warn=F)); close(mtaHdl); unlink(mtaFle); rm('mtaHdl', 'mtaFle');
+    xtdDta <- rjson::fromJSON(readLines(xtdHdl <- file(xtdFle <- unzip(fleNme, 'xdata.json',           junkpaths = T), 'r'), warn=F)); close(xtdHdl); unlink(xtdFle); rm('xtdHdl', 'xtdFle');
+                                        binHdl <- file(binFle <- unzip(fleNme, 'data.bin',             junkpaths = T), 'rb');
+    if (strBin)                       { strHdl <- file(strFle <- unzip(fleNme, 'strings.bin',          junkpaths = T), 'rb'); }
 
-    mnfTxt <-          readLines(mnfHdl <- file(mnfFle <- unzip(fleNme, 'META-INF/MANIFEST.MF', junkpaths = T), 'r'), warn=F);  close(mnfHdl); unlink(mnfFle); rm('mnfHdl', 'mnfFle');
-    mtaDta <- fromJSON(readLines(mtaHdl <- file(mtaFle <- unzip(fleNme, 'metadata.json',        junkpaths = T), 'r'), warn=F)); close(mtaHdl); unlink(mtaFle); rm('mtaHdl', 'mtaFle');
-    xtdDta <- fromJSON(readLines(xtdHdl <- file(xtdFle <- unzip(fleNme, 'xdata.json',           junkpaths = T), 'r'), warn=F)); close(xtdHdl); unlink(xtdFle); rm('xtdHdl', 'xtdFle');
-                                 binHdl <- file(binFle <- unzip(fleNme, 'data.bin',             junkpaths = T), 'rb');
-    # TO-DO: import HTML output - index.html
+    # TO-DO: decode the manifest file
+    # TO-DO: import and decode HTML output - index.html
    
     # process meta-data
-    if (names(mtaDta) != "dataSet") { stop('Unimplemeted field in the meta data') }
+    if (any(names(mtaDta) != "dataSet")) { stop('Unimplemeted field in the meta data') }
+
     # rowCount, columnCount
     rowNum = mtaDta$dataSet$rowCount
     colNum = mtaDta$dataSet$columnCount
-    # TO-DO: removedRows, addedRows 
-    if (length(mtaDta$dataSet$removedRows) > 0 || length(mtaDta$dataSet$addedRows) > 0) { stop('addedRow or removedRows not empty: Needs to be implemented') }
+    if (length(mtaDta$dataSet$fields) != colNum) { stop('Number of fields in the metadata is not matching up the number of columns.') }
 
-    # fields
-    nmeLst = c()
+    # iterate through fields
     lblLst = c()
     fltLst = c()
-    for (i in 1:length(mtaDta$dataSet$fields)) {
+    for (i in 1:colNum) {
         # type: determines the format in the binary file
         if      (mtaDta$dataSet$fields[[i]]$type == 'integer') { 
-            colRaw = as.data.frame(readBin(binHdl, integer(), n = rowNum))
+            colRaw = as.data.frame(readBin(binHdl,   integer(), n = rowNum))
         } else if (mtaDta$dataSet$fields[[i]]$type == 'number') { 
-            colRaw = as.data.frame(readBin(binHdl,  double(), n = rowNum))
+            colRaw = as.data.frame(readBin(binHdl,    double(), n = rowNum))
+        } else if (mtaDta$dataSet$fields[[i]]$type == 'string') {
+            colRaw = as.data.frame(readBin(strHdl, character(), n = rowNum))
+                                   readBin(binHdl,   integer(), n = rowNum)
         } else {
             stop(paste('Variable type', mtaDta$dataSet$fields[[i]]$type, 'not implemented.'))
         }
-        
+        print(str(colRaw))
+
         # name, description
         nmeCrr = mtaDta$dataSet$fields[[i]]$name
         lblCrr = mtaDta$dataSet$fields[[i]]$description        
-        nmeLst = c(nmeLst, nmeCrr)
         lblLst = c(lblLst, lblCrr)
-        if (lblCrr != "")
-            attr(colRaw[[1]], 'jmv-desc') = lblCrr
-        # dataType - value labels
-        if (any(grepl(nmeCrr, names(xtdDta)))) {
-            if (mtaDta$dataSet$fields[[i]]$dataType == 'Text') {
+        
+        # value labels
+        if (any(nmeCrr == names(xtdDta))) {
+            if    (any(mtaDta$dataSet$fields[[i]]$columnType == c('Data', 'Recoded'))) {
                 colRaw[[1]] = factor(colRaw[[1]], levels = unlist(sapply(xtdDta[[nmeCrr]]$labels, function(m) m[1])), labels = unlist(sapply(xtdDta[[nmeCrr]]$labels, function(m) m[2])))
-            } else if (mtaDta$dataSet$fields[[i]]$columnType == 'Recoded') {
-                colRaw[[1]] = factor(colRaw[[1]], levels = unlist(sapply(xtdDta[[nmeCrr]]$labels, function(m) m[1])), labels = unlist(sapply(xtdDta[[nmeCrr]]$labels, function(m) m[2])))
-                attr(colRaw[[1]], 'values') = unlist(sapply(xtdDta[[nmeCrr]]$labels, function(m) as.integer(m[1])))
-            } else if (mtaDta$dataSet$fields[[i]]$columnType == 'Filter') {
+                if (mtaDta$dataSet$fields[[i]]$dataType == 'Integer') { 
+                    attr(colRaw[[1]], 'values') = unlist(sapply(xtdDta[[nmeCrr]]$labels, function(m) as.integer(m[1])))
+                }
+            } else if (mtaDta$dataSet$fields[[i]]$columnType ==   'Filter') {
                 colRaw[[1]] = as.logical(colRaw[[1]])
                 fltLst = c(fltLst, i)
             } else {
-                print('HERE')
                 print(paste(nmeCrr, ' - ', mtaDta$dataSet$fields[[i]]$dataType, ' - ', mtaDta$dataSet$fields[[i]]$columnType))
             }
         }
         
-        # TO-DO: id, columnType, measureType, formula, formulaMessage, parentId, width, importName, transform, edits, missingValues, trimLevels
-        str(colRaw)
-        if (i == 1) { names(colRaw) = nmeCrr; dtaFrm = colRaw } else { dtaFrm[nmeCrr] = colRaw }
-        print(i)
-        str(dtaFrm)
+        if (i == 1) { 
+            names(colRaw) = nmeCrr
+            dtaFrm = colRaw
+        } else { 
+            dtaFrm[nmeCrr] = colRaw
+        }
+
+        if (lblCrr != "")
+            attr(dtaFrm[[nmeCrr]], 'jmv-desc') = lblCrr
+            
+        if (mtaDta$dataSet$fields[[i]]$measureType == "ID") {
+            attr(dtaFrm[[nmeCrr]], 'jmv-id') = T
+        }
+        
+        if (length(mtaDta$dataSet$fields[[i]]$missingValues) > 0) {
+            attr(dtaFrm[[nmeCrr]], 'missingValues') = mtaDta$dataSet$fields[[i]]$missingValues
+        }
+        
+        if (sveAtt) {
+            for (attNme in c('id', 'columnType', 'dataType', 'measureType', 'formula', 'formulaMessage', 'parentId', 'width', 'importName', 'transform', 'edits', 'trimLevels')) {
+                attr(dtaFrm[[nmeCrr]], attNme) = mtaDta$dataSet$fields[[i]][[attNme]]
+            }
+        }
+
+        if (rmMsVl) {
+            mssLst = attr(dtaFrm[[nmeCrr]], 'missingValues')
+            if (length(mssLst) > 0) {
+               attCrr = attributes(dtaFrm[[nmeCrr]])
+               rmvLvl = rep(FALSE, length(levels(dtaFrm[[nmeCrr]])))
+               for (j in 1:length(mssLst)) {
+                   dtaFrm[[nmeCrr]][eval(parse(text = paste0('dtaFrm[["', nmeCrr, '"]]', mssLst[[j]])))] <- NA
+                   rmvLvl <- rmvLvl | eval(parse(text = paste0('levels(dtaFrm[["', nmeCrr, '"]]) ', mssLst[j])))
+               }
+               dtaFrm[[nmeCrr]] = dtaFrm[[nmeCrr]][, drop = TRUE]
+               attCrr$missingValues = list()
+               attCrr$values <- attCrr$values[!rmvLvl]
+               for (attNme in setdiff(names(attCrr), names(attributes(dtaFrm[[nmeCrr]])))) {
+                   attr(dtaFrm[[nmeCrr]], attNme) <- attCrr[[attNme]]
+               }
+            }
+        }
+        
         rm('colRaw')
     }
+    
+    # close and remove the binary file(s)
+    close(binHdl); unlink(binFle); rm('binHdl', 'binFle');
+    if (strBin) {
+        close(strHdl); unlink(strFle); rm('strHdl', 'strFle');
+    }    
     
     # handle filters
     if (useFlt) {
         fltInc = rep(TRUE, dim(dtaFrm)[1])
         for (i in fltLst) fltInc = fltInc & dtaFrm[[i]]
-        print(str(dtaFrm))
+        str(dtaFrm)
         dtaFrm = dtaFrm[fltInc, ]
-        print(str(dtaFrm))
-#       dtaFrm[fltLst] = NULL
-        print(str(dtaFrm))
-    } else {
-        attr(dtaFrm, 'fltLst') = nmeLst[fltLst]
-    }   
+        str(dtaFrm)
+        dtaFrm[fltLst] = NULL
+        str(dtaFrm)
+    } else if(length(fltLst) > 0) {
+        attr(dtaFrm, 'fltLst') = names(dtaFrm)[fltLst]
+    }
     
-    print(lblLst);
-    # handle variable labels
-    names(lblLst) = nmeLst; attr(dtaFrm, 'variable.labels') = lblLst; rm('nmeLst', 'lblLst');   
-            
-    # TO-DO: convert transforms into attributes
-    # 'name', 'id', 'suffix', 'formula', 'formulaMessage', 'measureType', 'description'
+    # handle variable labels: R-foreign-style
+    if (! all(lblLst == "")) {
+        names(lblLst) <- names(dtaFrm)
+        attr(dtaFrm, 'variable.labels') = lblLst
+        rm('lblLst');
+    }
     
-    # close and remove the binary file
-    close(binHdl); unlink(binFle); rm('binHdl', 'binFle');
+    # removedRows, addedRows, transforms
+    if (sveAtt) {
+        for (attNme in c('removedRows', 'addedRows', 'transforms')) {
+            attr(dtaFrm, attNme) = mtaDta$dataSet[[attNme]]
+        }
+    }
     
-    # TO-DO: decode the manifest file
     # TO-DO: import HTML output - index.html
    
     # return the resulting data frame
-    dtaFrm      
+    dtaFrm
 }
---
+++ R/jmvReadWrite.R
@@ -0,0 +1,17 @@
+#' jmvReadWrite: A package to read and write .omv-files (jamovi statistical spreadsheet, www.jamovi.org).
+#'
+#' The jmvReadWrite package provides two functions:
+#' jmvRead and jmvWrite.
+#' 
+#' @section jmvReadWrite functions:
+#' The package jmvReadWrite reads and write the files that are produced of the statistical spreadsheet jamovi (www.jamovi.org).
+#' As syntax from jamovi can also be directly used in R, having import / export routines for the data files permits an easy
+#' transfer of analyses between jamovi and R.
+#'
+#' @docType package
+#' @name jmvReadWrite
+#' 
+#' @importFrom stats sd
+#' @importFrom utils str unzip zip
+#' @importFrom rjson fromJSON toJSON
+NULL

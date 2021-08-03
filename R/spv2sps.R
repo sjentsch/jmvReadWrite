2762c6072f18ae12d590afc21e58024d02ad2295
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2021-08-03 13:37:02 +0200

    Improved syntax extraction from SPV-files - works now stable with ~300 test files

--- R/spv2sps.R
+++ R/spv2sps.R
@@ -13,19 +13,23 @@ spv2sps <- function(fleSPV = "") {
     fleSPV <- normalizePath(fleSPV);
     hdrStr <- readBin(tmpHdl <- file(fleSPV, 'rb'), 'character'); close(tmpHdl); rm('tmpHdl');
     if (! hdrStr == "PK\003\004\024")                                     { stop(paste0('File "', fleSPV, '" has not the correct file format (is not a ZIP archive).')) }
-    zipLst = unzip(fleSPV, list=TRUE)$Name;
+    lstZIP = unzip(fleSPV, list=TRUE)$Name;
 
     # extract the syntax from the XML-files where the different elements of the
     # output are stored (those files start with 'outputViewer' followed by
     # numbers and the .xml-file-extension); the text with the syntax is just
     # HTML-code (the formatting nd the HTML-tags are removed)
     txtSPS = c();
-    logLst = zipLst[grepl('outputViewer[[:digit:]]+\\.xml', zipLst)];
+    logLst = lstZIP[grepl('outputViewer[[:digit:]]+\\.xml', lstZIP)];
     for (logNme in logLst) {
-        logTxt <- readLines(logHdl <- file(logFle <- unzip(fleSPV, logNme, junkpaths = TRUE), 'r'),  warn = FALSE); close(logHdl); unlink(logFle); rm('logHdl', 'logFle');
-        txtSPS <- c(txtSPS, unlist(strsplit(gsub("\\s+", " ", gsub('&#160;', ' ', trimws(gsub("<.*?>", " ", logTxt[grepl('</font>$', logTxt)])))), '\\. ')));
+        logTxt <- rawToChar(readBin(logHdl <- file(logFle <- unzip(fleSPV, logNme, junkpaths = TRUE), 'rb'), 'raw', file.info(logFle)$size)); close(logHdl); unlink(logFle); rm('logHdl', 'logFle');
+        crrSPS <- gsub('\\s+', ' ', gsub('\\n', ' ', gsub('&nbsp;', ' ', gsub('\ua0', ' ', gsub('&#160;', ' ', trimws(gsub('<.*?>', ' ', strsplit(strsplit(strsplit(strsplit(logTxt, '<container')[[1]][2], '<!\\[CDATA\\[')[[1]][2], '\\]\\]>')[[1]][1], '</head>')[[1]][2])))))));
+        txtSPS <- c(txtSPS, unlist(strsplit(crrSPS, '\\. ')));
     }
     txtSPS = as.list(gsub('\\.\\.', '\\.', paste0(txtSPS, '.')));
+    txtSPS[grep('DATASET ',      txtSPS)] <- NULL;
+    txtSPS[grep('GET FILE=',     txtSPS)] <- NULL;    
+    txtSPS[grep('SAVE OUTFILE=', txtSPS)] <- NULL;
     
     # the data file (.sav) that was used for a particular analysis is stored in
     # files whose file names begin with numbers and end with lightNotesData.xml
@@ -34,11 +38,11 @@ spv2sps <- function(fleSPV = "") {
     # the string '.sav' followed by character(0) which is the end of the file
     # name; the begin is another character(0)
     fleSAV = c();
-    nteLst = zipLst[grepl('[[:digit:]]+_lightNotesData\\.bin', zipLst)];
+    nteLst = lstZIP[grepl('[[:digit:]]+_lightNotesData\\.bin', lstZIP) | grepl('[[:digit:]]+_notesData\\.bin', lstZIP)];
     for (nteNme in nteLst) {
         nteBin <- readBin(nteHdl <- file(nteFle <- unzip(fleSPV, nteNme, junkpaths = TRUE), 'rb'), 'raw', file.info(nteFle)$size); close(nteHdl); unlink(nteFle); rm('nteHdl', 'nteFle');
-        savPos = min(which(nteBin[seq(1, length(nteBin) - 4)] == charToRaw('.') & nteBin[seq(2, length(nteBin) - 3)] == charToRaw('s') & nteBin[seq(3, length(nteBin) - 2)] == charToRaw('a') & nteBin[seq(4, length(nteBin) - 1)] == charToRaw('v') & nteBin[seq(5, length(nteBin))] == '00'));
-        fleSAV = c(fleSAV, rawToChar(nteBin[seq(max(which(nteBin[seq(1, savPos)] == '00')) + 1, savPos + 3)]));
+        savPos = nteBin[seq(1, length(nteBin) - 4)] == charToRaw('.') & nteBin[seq(2, length(nteBin) - 3)] == charToRaw('s') & nteBin[seq(3, length(nteBin) - 2)] == charToRaw('a') & nteBin[seq(4, length(nteBin) - 1)] == charToRaw('v') & (nteBin[seq(5, length(nteBin))] == '00' | nteBin[seq(5, length(nteBin))] == '01');
+        if (any(savPos)) fleSAV = c(fleSAV, gsub('\\\\', '/', rawToChar(nteBin[seq(max(which(nteBin[seq(1, min(which(savPos)))] == '00')) + 1, min(which(savPos)) + 3)])));
     }
     # check whether exactly one data file was used for all analyses in the
     # .spv-file; if so, return the file name as atrribute 'datafile' of txtSPS;
@@ -53,10 +57,12 @@ spv2sps <- function(fleSPV = "") {
         } else if (file.exists(file.path(dirname(fleSPV), basename(fleSAV)))) {
             attr(txtSPS, 'datafile') <- file.path(dirname(fleSPV), basename(fleSAV));
         } else {
-            stop(paste0('SPSS data file \"', basename(fleSAV), '\" not found: It is expected to be found either at the position that is stored in the .spv-file, the current working directory, or the directory where the .spv-file was located. Please correct it (copy the data file to one of these places) and run the function again.'));
+            stop(paste0('SPSS data file \"', basename(fleSAV), '\" not found: It is expected to be found either at the position that is stored in the .spv-file, the current working directory, or the directory where the .spv-file was located (', fleSPV, '). Please correct it (copy the data file to one of these places) and run the function again.'));
         }
+    } else if (length(fleSAV) == 0) {
+        warning('The name of the data file was unavailable / not stored (e.g., because you worked with a dataset with values that were typed in but not stored before conducting the analyses).');
     } else {
-        warning('File name of the data file insufficient: Either there was more than one data file used in the analyses that are stored in the .spv-file or the file was not stored (e.g., because you worked with a dataset with values that were typed in but not stored before conducting the analyses).');
+        warning(paste('There was more than one data file used in the analyses that are stored in the .spv-file:', paste(as.character(fleSAV), collapse=", ")));
     }
 
     # return the syntax

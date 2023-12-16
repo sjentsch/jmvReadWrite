e26dc72e0ca2ad743c42d418dd9491ae0774bdc3
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2023-12-16 20:24:59 +0100

    Change return value to invisible NULL

--- R/describe_omv.R
+++ R/describe_omv.R
@@ -104,7 +104,7 @@ describe_omv <- function(dtaInp = NULL, fleOut = "", dtaTtl = c(), dtaDsc = c(),
 
     if (utils::packageVersion("jmvcore") < "2.4.3") {
         warning("jmvcore version 2.4.3 (or higher) is required for using describe_omv.\n\n")
-        return()
+        return(invisible(NULL))
     }
     if (!jmvPtB()) stop("The R-packages RProtoBuf and jmvcore must be installed for using describe_omv (see warnings() for further details).")
 
@@ -319,7 +319,7 @@ prpAtt <- function(lstAtt = NULL) {
     crrAtt <- lstAtt
     if (utils::hasName(lstAtt, "list") && identical(attr(lstAtt[["list"]], "lstEnt"), TRUE)) crrAtt[["list"]] <- NULL
     if (utils::hasName(lstAtt, "formula")) crrAtt[["formula"]] <- NULL
-    if (length(crrAtt) > 0) return(list(attributes = crrAtt)) else return(NULL)
+    if (length(crrAtt) > 0) return(list(attributes = crrAtt)) else return(invisible(NULL))
 }
 
 # add / replace HTML for title and description within index.html

a1af57730918435d4fbf5330f40d347f37861305
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2024-09-29 13:52:59 +0200

    Added function torsos (variable description, etc.) for distances_omv and proximities_omv (functionality to be added)

---
+++ R/distances_omv.R
@@ -0,0 +1,60 @@
+#' Calculates distances (returns symmetric matrix) from a raw data matrix in .omv-files for the statistical spreadsheet 'jamovi' (<https://www.jamovi.org>)
+#'
+#' @param dtaInp Either a data frame or the name of a data file to be read (including the path, if required; "FILENAME.ext"; default: NULL); files can be of
+#'               any supported file type, see Details below
+#' @param fleOut Name of the data file to be written (including the path, if required; "FILE_OUT.omv"; default: ""); if empty, the resulting data frame is
+#'               returned instead
+#' @param varDst Variable (default: c()) containing a character vector with the names of the variables for which distances are to be calculated. See Details
+#'               for more information.
+#' @param nmeDst Variable (default: "euclidean") containing a character indicating which distance measure is to be calculated calculated. See Details for more
+#'               information.
+#' @param clmDst Whether the distances shall be calculated between columns (TRUE) or rows (FALSE; default: TRUE). See Details for more information.
+#' @param mtxSps Whether the symmetric matrix to be returned should be sparse (default: FALSE)
+#' @param mtxTrL Whether the symmetric matrix to be returned should only contain the lower triangular (default: FALSE)
+#' @param mtxDgn Whether the symmetric matrix to be returned should retain the values in the main diagonal (default: TRUE)
+#' @param usePkg Name of the package: "foreign" or "haven" that shall be used to read SPSS, Stata and SAS files; "foreign" is the default (it comes with
+#'               base R), but "haven" is newer and more comprehensive
+#' @param selSet Name of the data set that is to be selected from the workspace (only applies when reading .RData-files)
+#' @param ...    Additional arguments passed on to methods; see Details below
+#'
+#' @return a data frame containing a symmetric matrix (only returned if `fleOut` is empty) where the order of variables / columns of the input data set is
+#'         re-arranged
+#'
+#' @details
+#' * `varDst` must a character vector containing the variables to calculated distances over. If `clmDst` is set to TRUE, distances are calculated between all
+#'   possible variable pairs and over subjects / rows in the original data frame. If `clmDst` is set to FALSE, distances are calculated between participants
+#'   and over all variables given in `varDst`.
+#' * `nmeDst` can be one of the following distance measures: ``
+#' * If `clmDst` is set to `TRUE`, the symmetric matrix that is returned has the size V x V (V being the number of variables in varDst; if `mtxSps` is set to
+#'   `TRUE`, the size is V - 1 x V - 1, see below); if `clmDst` is set to `FALSE`, the symmetric matrix that is returned has the size R x R (R being the number
+#'   of rows in the original dataset; it is if `mtxSps` is set to `TRUE`, the size is R - 1 x R - 1, see below).
+#' * If `mtxSps` is set, a sparse matrix is returned. Those matrices are similar to the format one often finds for correlation matrices. The values are only
+#'   retained in the lower triangular, the columns range from the first to the variable that is second to the last in `varDst` (or respectively, the columns
+#'   contain the first to the second to the last row of the original dataset when `clmDst` is set to `FALSE`), and the rows contain the second to the last
+#'   variable in `varDst` (or respectively, the rows contain the second to the last row of the original dataset when `clmDst` is set to `FALSE`).
+#' * By default, a full symmetric matrix is returned (i.e., a matrix that has no NAs in any cell). This behaviour can be changed with setting `mtxTrL` and
+#'   `mtxDgn`: If `mtxTrL` is set to `TRUE`, the values from the upper triangular matrix are removed / replaced with NAs; if `mtxDgn` is set to `FALSE`, the
+#'   values from the main diagonal are removed / replaced with NAs.
+#' * The ellipsis-parameter (`...`) can be used to submit arguments / parameters to the functions that are used for reading and writing the data. By clicking
+#'   on the respective function under “See also”, you can get a more detailed overview over which parameters each of those functions take. The functions are:
+#'   `read_omv` and `write_omv` (for jamovi-files), `read.table` (for CSV / TSV files; using similar defaults as `read.csv` for CSV and `read.delim` for TSV
+#'   which both are based upon `read.table`), `load` (for .RData-files), `readRDS` (for .rds-files), `read_sav` (needs the R-package `haven`) or `read.spss`
+#'   (needs the R-package `foreign`) for SPSS-files, `read_dta` (`haven`) / `read.dta` (`foreign`) for Stata-files, `read_sas` (`haven`) for SAS-data-files,
+#'   and `read_xpt` (`haven`) / `read.xport` (`foreign`) for SAS-transport-files. If you would like to use `haven`, you may need to install it using
+#'   `install.packages("haven", dep = TRUE)`.
+#'
+#' @seealso `distances_omv` internally uses the following functions for reading and writing data files in different formats: [jmvReadWrite::read_omv()] and
+#'   [jmvReadWrite::write_omv()] for jamovi-files, [utils::read.table()] for CSV / TSV files, [load()] for reading .RData-files, [readRDS()] for .rds-files,
+#'   [haven::read_sav()] or [foreign::read.spss()] for SPSS-files, [haven::read_dta()] or [foreign::read.dta()] for Stata-files, [haven::read_sas()] for
+#'   SAS-data-files, and [haven::read_xpt()] or [foreign::read.xport()] for SAS-transport-files.
+#'
+#' @examples
+#' \dontrun{
+#' }
+#'
+#' @export distances_omv
+#'
+distances_omv <- function(dtaInp = NULL, fleOut = "", varDst = c(), nmeDst = c("euclidean"), clmDst = TRUE, mtxSps = FALSE, mtxTrL = FALSE, mtxDgn = TRUE,
+                          usePkg = c("foreign", "haven"), selSet = "", ...) {
+# TBA
+}
